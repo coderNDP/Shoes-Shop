8e2c2d7e178dfd08b4b61a3d0187d6a09619f873
coderNDP <nguyenphuduy2004@gmail.com>
2023-12-19 15:35:34 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,91 @@
+<?php
+  include_once "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+  <style>
+    .box3{
+      width: 100%;
+      height: 100%;
+      position: absolute;
+    }
+    
+    .box1{
+      width: 49%;
+      float:left;
+      position: relative;
+    }
+    ul li span{
+      font-size: 20px;
+    }
+   .title_db span{
+      color: #444444;
+font-size: 25px;
+font-weight: 700;
+letter-spacing: 0.18px;
+line-height: 25.2px;
+padding: 10px 0px;
+text-transform: uppercase;
+    }
+    div{
+      color: #444444;
+font-size: 14px;
+line-height: 23.8px;
+padding: 0px 15px
+    }
+    .box2{
+      width: 49%;
+      position: relative;
+      float:right;
+    }
+  </style>
+</head>
+<body>
+  <div class="box3">
+<div class="box1">
+					<div class="page_cotact">
+						<h1 class="title_db"><span>Sports Sneakers</span></h1>
+					</div>
+					<div class="logocontact">
+						<a href="/" class="logo-wrapper ">					
+							<img src="img/logo-1.png" alt="logo" width="100px" height="100px">					
+						</a>
+					</div>
+					<p class="footer_des_contact">
+						
+					</p>
+					<ul class="list_menu_update2 cf">
+						<li>
+							<span class="icon_fl"><i class="fa fa-map-marker"></i></span>
+							<span class="icon_fr"><span>Số 1 Đại Cồ Việt , Hai Bà Trưng, Hà Nội </span></span>
+						</li>
+						<li>
+
+							<span class="icon_fl"><i class="fa fa-phone"></i></span>
+							<span class="icon_fr">
+								<span>
+									 0976 036 251 
+								</span>
+							</span>
+						</li>
+						<li>
+							<span class="icon_fl"><i class="fa fa-envelope"></i></span>
+							<span class="icon_fr">
+								<span>
+									 sport@sneakers.vn 
+								</span>
+							</span>
+						</li>
+					</ul>
+				</div>
+
+<iframe class="box2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.498668525232!2d105.83039901423427!3d21.01272399370298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab299df9ef93%3A0x573a54b8992204b0!2zVmFucyBTdG9yZSA5NyDEkOG6t25nIFbEg24gTmfhu68!5e0!3m2!1svi!2s!4v1576050544036!5m2!1svi!2s" width="800" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+  </div>
+</body>
+</html>
+
+
